ce5ebb03f2376006019b85b59119319571578fd0
OceanandWild <149962867+OceanandWild@users.noreply.github.com>
2025-02-25 21:08:26 -0300

    Add files via upload

---
+++ productServer.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'hachiyt001@gmail.com',
+        pass: 'nkqv wmet yusf znja'
+    }
+});
+
+app.post('/createProduct', (req, res) => {
+    const { productName, productDescription, productPrice, userEmail } = req.body;
+
+    const mailOptions = {
+        from: 'hachiyt001@gmail.com',
+        to: 'hachiyt001@gmail.com',
+        subject: 'Nuevo Producto Creado',
+        text: `Nombre del Producto: ${productName}\nDescripción: ${productDescription}\nPrecio: ${productPrice}\nCorreo del Usuario: ${userEmail}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).json({ success: false, error: error.message });
+        }
+        res.json({ success: true });
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000');
+});
